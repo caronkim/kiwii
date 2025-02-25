16bc1797328e9d38e83dd65768b74deab5083dc2
jongwooDaou <jong168@daou.ac.kr>
2025-02-25 14:29:01 +0900

    Point history

---
+++ src/main/java/org/example/kiwii/dao/point/PointDAO.java
@@ -0,0 +1,28 @@
+package org.example.kiwii.dao.point;
+
+import org.apache.ibatis.session.SqlSession;
+import org.example.kiwii.vo.point.PointHistoryVO;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PointDAO {
+    private final SqlSession sqlSession;
+
+    public PointDAO(SqlSession sqlSession) {
+        this.sqlSession = sqlSession;
+    }
+
+    public List<PointHistoryVO> selectPointHistoryByUserUUID(int uuid) {
+        List<PointHistoryVO> list = new ArrayList<>();
+
+        try{
+            list = sqlSession.selectList("PointHistory.selectPointHistoryByUserUUID", uuid);
+            return list;
+        } catch (Exception e){
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+}
