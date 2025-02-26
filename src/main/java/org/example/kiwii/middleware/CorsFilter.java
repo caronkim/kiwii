e1031cd492680dda5ee87756fb8881acf224d126
daou_tlscjfghks <tlscjfghks@daou.co.kr>
2025-02-26 17:00:57 +0900

    change CorsFilter

--- src/main/java/org/example/kiwii/middleware/CorsFilter.java
+++ src/main/java/org/example/kiwii/middleware/CorsFilter.java
@@ -2,6 +2,7 @@ package org.example.kiwii.middleware;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
@@ -15,9 +16,16 @@ public class CorsFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
         HttpServletResponse httpResponse = (HttpServletResponse) response;
 
-        httpResponse.setHeader("Access-Control-Allow-Origin", "*");
+        String origin = httpRequest.getHeader("Origin");
+
+        if (origin != null) {
+            httpResponse.setHeader("Access-Control-Allow-Origin", origin);
+            httpResponse.setHeader("Access-Control-Allow-Credentials", "true");
+        }
+
         httpResponse.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         httpResponse.setHeader("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, Authorization");
         httpResponse.setHeader("Access-Control-Allow-Credentials", "true");
